c0402e01073ef2748f1cece10b285e05c9d27cf8
kevin.muppattayil <kevin.muppattayil@infosys.com>
2018-05-22 12:43:00 -0400

    file upload: Permission Error

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask
-
+from flask import Flask, request, abort, jsonify
+import requests
+import os
 app = Flask(__name__)
+
+file_name_counter = 0
 
 
 @app.route('/')
 def index():
     return 'Hello World'
+
+
+@app.route('/Upload', methods=['GET', 'POST'])
+def verify():
+    if request.method == 'POST':
+        if not request.files:
+            abort(400)
+
+        data = {"user_id": request.headers["user_id"],
+                "room_id": request.headers["room_id"],
+                "action": request.headers["action"]}
+
+        print(data)
+        image = request.files['file']
+        image.save('C:\\Users\\kevin.muppattayil\\Documents', ('image_' + str(file_name_counter)))
+
+    return jsonify({'status': 'success'}), 200
 
 
 if __name__ == '__main__':
