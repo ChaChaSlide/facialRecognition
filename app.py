4180cea9da1e2217c06b5daf3853fc5b674d57f7
kevin.muppattayil <kevin.muppattayil@infosys.com>
2018-05-23 10:59:13 -0400

    clean up and some safety on upload and host

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, request, abort, jsonify, send_from_directory
-import requests
-import os
+from flask import Flask, request, abort, jsonify, send_from_directory, url_for
 app = Flask(__name__)
 
 file_name_counter = 0
@@ -13,26 +11,26 @@ def index():
 
 @app.route('/Upload', methods=['GET', 'POST'])
 def upload():
-    if request.method == 'POST':
-        if not request.files:
-            abort(400)
+    if not request.files or not request.headers["user_id"] or not request.headers["room_id"] or not request.headers["action"]:
+        abort(400)
 
-        data = {"user_id": request.headers["user_id"],
-                "room_id": request.headers["room_id"],
-                "action": request.headers["action"]}
+    data = {"user_id": request.headers["user_id"],
+            "room_id": request.headers["room_id"],
+            "action": request.headers["action"]}
 
-        print(data)
-        image = request.files['file']
-        global file_name_counter
-        filename = str(file_name_counter) + '.' + image.filename.rsplit('.', 1)[1].lower()
-        file_name_counter += 1
-        image.save('static\image_' + filename)
+    image = request.files['file']
+    global file_name_counter
+    filename = 'image_' + str(file_name_counter) + '.' + image.filename.rsplit('.', 1)[1].lower()
+    file_name_counter += 1
+    image.save('static\\' + filename)
 
+    data["image_id"] = url_for('hosted_image', image_id=filename)
+    print(data)
     return jsonify({'status': 'success', 'image_id': filename}), 200
 
 
 @app.route('/images/<image_id>')
-def host_image(image_id):
+def hosted_image(image_id):
     return send_from_directory('static/', image_id), 200
 
 
