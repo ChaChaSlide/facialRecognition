1110fc9d06dd1a330cc9a2d95266d3a87ae5672b
Gitesh Agarwal <gagarwal916@gmail.com>
2018-06-06 13:01:50 -0400

    GAE configs and modifications

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, abort, jsonify, send_from_directory, url_for
 import os
+from google.cloud import storage
+
+
 app = Flask(__name__)
 
-#app.config['SERVER_NAME'] = 'infosys-facial-recognition.appspot.com'
+CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
 
 file_name_counter = 0
 
@@ -17,18 +20,23 @@ def index():
 @app.route('/Upload', methods=['POST'])
 def upload():
     if not request.files or not request.headers["room_id"] or not request.headers["action"]:
-        abort(402)
+        abort(406)
 
     image = request.files['file']
     global file_name_counter
     filename = 'image_' + str(file_name_counter) + '.' + image.filename.rsplit('.', 1)[1].lower()
     file_name_counter += 1
-    image.save('static\\' + filename)
+#    image.save(filename)
+
+    gcs = storage.Client()
+    bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+    blob = bucket.blob(filename)
+    blob.upload_from_file(image)
 
     if request.headers["action"].lower() == 'enroll':
         if not request.headers["user_id"]:
-            abort(402)
-        print('http://' + app.config['SERVER_NAME'] + url_for('hosted_image', image_id=filename))
+            abort(406)
+        
         global database
         if request.headers['name']:
             database[request.headers['user_id']] = {'name': request.headers["name"],
@@ -41,14 +49,14 @@ def upload():
     elif request.headers["action"].lower() == 'recognize':
         print('http://' + url_for('hosted_image', image_id=filename))
     else:
-        abort(402)
+        abort(406)
 
-    return jsonify({'status': 'success', 'image_id': filename}), 200
+    return jsonify({'status': 'success', 'image_id': blob.public_url}), 200
 
 
 @app.route('/images/<image_id>')
 def hosted_image(image_id):
-    return send_from_directory('static/', image_id), 200
+    return send_from_directory('/', image_id), 200
 
 
 if __name__ == '__main__':
