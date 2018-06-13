806538b4da6b77bbb1a93742dcef5946f4e578e3
KevinM-Infosys <kevin.muppattayil@infosys.com>
2018-06-13 12:10:18 -0400

    anfernee's code

--- karios_interface.py
+++ karios_interface.py
@@ -1,5 +1,7 @@
 import requests
-from flask import Flask, render_template, request, json
+from flask import Flask, json
+
+app = Flask(__name__)
 
 api_url = "https://api.kairos.com"
 app_id = "01d28598"
@@ -12,7 +14,11 @@ headers = {
     "app_key": app_key
 }
 
+@app.route('/')
+def index():
+    return 'Hello World!'
 
+@app.route('/recognize')
 def recognize(image_url):
     values = {
             'image': image_url,
@@ -20,21 +26,23 @@ def recognize(image_url):
         }
     request = requests.post('https://api.kairos.com/recognize', data=bytes(json.dumps(values), encoding="utf-8"), headers=headers)
     json_data = json.loads(request.text)
-    results = json_data['images'][0]
-    count = 0
     resultList = []
-    resultID = []
-    resultConfidence = []
-    #print(results['candidates'])
-    for i in results['candidates']:
-        if results['candidates'][count]['confidence'] > .60:
-            resultID.append(results['candidates'][0]['subject_id'])
-            resultConfidence.append(results['candidates'][0]['confidence'])
-        count+=1
-    resultList = list(zip(resultID,resultConfidence))
-    print(resultList)
-    return resultList ;
+    try:
+        results = json_data['images'][0]
+        count = 0
+        resultID = []
+        resultConfidence = []
 
+        for i in results['candidates']:
+           if results['candidates'][count]['confidence'] > .60:
+                resultID.append(results['candidates'][0]['subject_id'])
+                resultConfidence.append(results['candidates'][0]['confidence'])
+           count+=1
+        resultList = list(zip(resultID, resultConfidence))
+    except KeyError:
+        return resultList
+    return (resultList);
+@app.route('/enroll')
 def enroll(image_url, subject_id):
     values = {
                 "image": image_url,
@@ -42,7 +50,14 @@ def enroll(image_url, subject_id):
                 "gallery_name": gallery
             }
 
-    request = Request('https://api.kairos.com/enroll', data=bytes(json.dumps(values), encoding="utf-8"), headers=headers)
-    response_body = urlopen(request).read()
-    print(response_body)
-    return response_body;
+    request = requests.post('https://api.kairos.com/enroll', data=bytes(json.dumps(values), encoding="utf-8"), headers=headers)
+    response_body = json.loads(request.text)
+    if len(response_body.keys()) > 1:
+        return True
+    else:
+        return False;
+
+enroll(image_url="http://media.kairos.com/kairos-lizabeth.jpg", subject_id="Elizabeth")
+#recognize(image_url="https://storage.googleapis.com/infosys-facial-recognition/image_2.jpg")
+if __name__ == '__main__':
+    app.run()
